6d1cd07b16348165444860910a12789975b3be1a
Vibhaj Rajan <vibhaj8@gmail.com>
2016-07-04 00:36:11 +0530

    added single local parallel for spec v1

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -6,56 +6,34 @@ use Behat\Behat\Context\BehatContext,
   Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
-use BrowserStack\Local;
 
-class FeatureContext extends BehatContext {
-  private $webDriver;
-  private $BROWSER_NAME = 'firefox';
-  private $OS = 'Windows';
-  private $OS_VERSION = '7';
-  private $BROWSER_VERSION = '23.0';
-  private $USERNAME = 'BROWSERSTACK_USERNAME'; // Set your username
-  private $BROWSERSTACK_ACCESS_KEY = 'BROWSERSTACK_ACCESS_KEY'; // Set your browserstack_access_key
-  private $tunnel = 'false';
-  private $bs_local;
-  public function __construct(array $parameters){
-    $this->tunnel = $parameters['tunnel'];
-    if ($this->tunnel == "true") {
-      $this->bs_local = new Local();
-      $bs_local_args = array("key" => 'BS_USERNAME', "forcelocal" => true);
-      $this->bs_local->start($bs_local_args);
-    }
-  }
-
-  public function __destruct() {
-    if ($this->tunnel == "true") {
-      $this->bs_local->stop();
-    }
-  }
-
+class FeatureContext extends BrowserStackContext {
   /** @Given /^I am on "([^"]*)"$/ */
   public function iAmOnSite($url) {
-    print("HHHHHHH >" . $this->tunnel);
-    $desiredCap =  array('browser'=> $this->BROWSER_NAME, 'browser_version'=> $this->BROWSER_VERSION, 'os' => $this->OS, 'os_version' => $this->OS_VERSION, 'browserstack.tunnel' => $this->tunnel);
-    $this->webDriver = RemoteWebDriver::create("http://".$this->USERNAME.":".$this->BROWSERSTACK_ACCESS_KEY."@hub.browserstack.com/wd/hub", $desiredCap);
-    $this->webDriver->get($url);
+    self::$driver->get($url);
   }
 
   /** @When /^I search for "([^"]*)"$/ */
   public function iSearchFor($searchText) {
-    $element = $this->webDriver->findElement(WebDriverBy::name("q"));
-    if ($element) {
-      $element->sendKeys($searchText);
-      $element->submit();
-    }
+    $element = self::$driver->findElement(WebDriverBy::name("q"));
+    $element->sendKeys($searchText);
+    $element->submit();
+    sleep(5);
   }
 
   /** @Then /^I get title as "([^"]*)"$/ */
   public function iShouldGet($string) {
-    $title = $this->webDriver->getTitle();
+    $title = self::$driver->getTitle();
     if ((string)  $string !== $title) {
-      throw new Exception("Actual output is:\n". $title);
+      throw new Exception("Expected title: '". $string. "'' Actual is: '". $title. "'");
     }
-    $this->webDriver->quit();
+  }
+
+  /** @Then /^I should see "([^"]*)"$/ */
+  public function iShouldSee($string) {
+    $source = self::$driver->getPageSource();
+    if (strpos($source, $string) === false) {
+      throw new Exception("Expected to see: '". $string. "'' Actual is: '". $source. "'");
+    }
   }
 }
